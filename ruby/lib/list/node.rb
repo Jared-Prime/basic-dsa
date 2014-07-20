8fd1e373194d35ce558a61c0a471486304d62f9c
Jared Davis <jared@haiq.us>
2014-07-20 10:21:28 -0500

    add List::Node implementation

---
+++ ruby/lib/list/node.rb
@@ -0,0 +1,9 @@
+module List
+  class Node
+    attr_accessor :data, :next_node, :prev_node
+
+    def initialize(data, next_node=nil, prev_node=nil)
+      @data, @next_node, @prev_node = data, next_node, prev_node
+    end
+  end
+end
