02c9657d73225dec4e99a9cef969b83317c3a8db
Jared Davis <jared@haiq.us>
2014-08-06 10:06:21 -0500

    [WIP] implement Tree:Binary
    Tree::Binary#find and Tree::Binary#insert operations

--- ruby/lib/tree/binary.rb
+++ ruby/lib/tree/binary.rb
@@ -9,8 +9,72 @@ module Tree
     end
 
     def insert(data)
-      node = Node.new data
-      @root = node unless @root
+      @root = create_node(@root, data)
+    end
+
+    def find(data)
+      find_node(@root, data)
+    end
+
+    def remove(data)
+      # search is required to find the next value in the binary search tree 
+      # that is compatible with the position of the node being removed. 
+      # When this is found, that value is replaced and the node found is removed.
+
+      # Green, Bradley; Brewer, Jason (2013-08-12).
+      # Programming Problems in Ruby (Kindle Locations 842-843). 
+    end
+
+    def find_parent(data)
+      return nil if @root.data == data
+      find_parent_node(@root, data)
+    end
+
+    def find_successor(data)
+      # The successor of a target node is a node in a binary search binary tree
+      # whose data would immediately follow the target node’s data in
+      # a sorted list of all the data members associated with the binary tree.
+
+      # Green, Bradley; Brewer, Jason (2013-08-12). 
+      # Programming Problems in Ruby (Kindle Locations 866-868). 
+    end
+
+    private
+
+    def find_parent_node(node, data)
+      if node.left_branch  && data == node.left_branch.data
+        return node
+      elsif node.right_branch && data == node.right_branch.data
+        return node
+      elsif data < node.data
+        return find_parent_node(node.left_branch, data)
+      elsif data >= node.data
+        return find_parent_node(node.right_branch, data)
+      else
+        return nil
+      end
+    end
+
+    def find_node(node, data)
+      return node if !node || node.data == data
+
+      if data < node.data
+        return find_node node.left_branch, data
+      else
+        return find_node node.right_branch, data
+      end
+    end
+
+    def create_node(node, data)
+      return Node.new(data) unless node
+
+      if data < node.data
+        node.left_branch  = create_node(node.left_branch, data)
+      else
+        node.right_branch = create_node(node.right_branch, data)
+      end
+
+      return node
     end
   end
 end
--- ruby/spec/tree/binary_spec.rb
+++ ruby/spec/tree/binary_spec.rb
@@ -22,12 +22,69 @@ describe Tree::Binary do
       subject.insert 5
       expect(subject.root.data).to be 42
       expect(subject.root.left_branch.data).to be 5
+      expect(subject.root.right_branch).to be_nil
+    end
+
+    it 'inserts larger values on the right branch' do
+      subject.insert 42
+      subject.insert 50
+      expect(subject.root.data).to be 42
+      expect(subject.root.left_branch).to be_nil
+      expect(subject.root.right_branch.data).to be 50
     end
   end
 
-  describe '#remove'
+  describe '#remove' do
+    before :each do
+      subject.insert 42
+      subject.insert 5
+      subject.insert 50
+      subject.insert 49
+      subject.insert 20
+    end
 
-  describe '#find'
+    it 'relies on search for parent node'
+    it 'relies on search for successor node'
+  end
+
+  describe '#find_parent' do
+    before :each do
+      subject.insert 42
+      subject.insert 5
+      subject.insert 50
+      subject.insert 49
+      subject.insert 20
+    end
+
+    it 'returns the parent node' do
+      expect(subject.find_parent(42)).to be_nil
+      expect(subject.find_parent(5).data).to be 42
+      expect(subject.find_parent(50).data).to be 42
+      expect(subject.find_parent(49).data).to be 50
+      expect(subject.find_parent(20).data).to be 5
+    end
+  end
+
+  describe '#find_successor'
+
+  describe '#find' do
+    before :each do
+      subject.insert 42
+      subject.insert 5
+      subject.insert 50
+      subject.insert 49
+      subject.insert 20
+    end
+
+    it 'recursively walks the tree to find the node' do
+      expect(subject.find(42)).to be_a Tree::Node
+      expect(subject.find(5)).to  be_a Tree::Node
+      expect(subject.find(50)).to be_a Tree::Node
+      expect(subject.find(49)).to be_a Tree::Node
+      expect(subject.find(20)).to be_a Tree::Node
+      expect(subject.find(11)).to be_nil
+    end
+  end
 
   describe '#balanced?'
 end
